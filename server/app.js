8e68e144c2202081a18a7077cff79fc0cd58008a
Denis <d.ivanitskiy-1994@yandex.ru>
2021-08-26 11:13:58 +0300

    express-routes-auth-create-test

--- server/app.js
+++ server/app.js
@@ -7,15 +7,15 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({
     extended: true
 }));
 app.use(express.json());
 app.use(morgan('dev'));
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 app.listen(PORT, ()=>{
-    console.log('Server has been started on PORT ' + PORT)
-})
+    console.log('Server has been started on PORT ' + PORT);
+});
