db43a719b06f82ef6fc83f74bccc0b7766acec75
Denis <d.ivanitskiy-1994@yandex.ru>
2021-08-26 13:58:34 +0300

    db-model-organization-&-routes-upd

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 const db = require('../db/models');
 
-router.get('/', async (req, res) => {
+router.post('/user', async (req, res) => {
+  console.log('signup');
+});
+
+router.post('/organization', async (req, res) => {
   console.log('signup');
 });
 
