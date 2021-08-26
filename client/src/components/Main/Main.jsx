91eccc98bb961e3fdbbc6842197efc63d1e8199b
Denis <d.ivanitskiy-1994@yandex.ru>
2021-08-26 15:44:44 +0300

    solve-conflicts

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -8,25 +8,47 @@ import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import Lk from '../Lk/Lk';
 
+import SignUpChoicePage from '../SignUpChoicePage/SignUpChoicePage';
+import SignUpUser from '../SignUpUser/SignUpUser';
+import SignUpOrganization from '../SignUpOrganization/SignUpOrganization';
+import SignInUser from '../SignInUser/SignInUser';
+import SignInOrganization from '../SignInOrganization/SignInOrganization';
+import SignInChoisePage from '../SignInChoicePage/SignInChoicePage';
+import Search from '../Search/Search';
+
 export default function Main() {
   return (
     <>
       <Router>
         <Header />
         <Switch>
-
-          <Route exact path="/">
-            {/*  */}
-          </Route>
-
           <Route exact path="/profile">
             <Lk />
           </Route>
-
+          <Route exact path="/">
+            <Search />
+          </Route>
+          <Route exact path="/signup">
+            <SignUpChoicePage />
+          </Route>
+          <Route exact path="/signup/user">
+            <SignUpUser />
+          </Route>
+          <Route exact path="/signup/organization">
+            <SignUpOrganization />
+          </Route>
+          <Route exact path="/signin">
+            <SignInChoisePage />
+          </Route>
+          <Route exact path="/signin/user">
+            <SignInUser />
+          </Route>
+          <Route exact path="/signin/organization">
+            <SignInOrganization />
+          </Route>
         </Switch>
         <Footer />
       </Router>
-
     </>
   );
 }
