5688e1c5883b300e9bf7f236870e9c9d58d14080
lil-m00ha <minaevaan95@gmail.com>
2021-08-31 17:45:59 +0300

    react-header has added profile logo

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -41,6 +41,7 @@ export default function Header() {
           </Link>
           <div>
             <Link className="headerLinkButton" to="/profile/user">{`${currentUser.firstName} ${currentUser.lastName}`}</Link>
+            <img src={`${process.env.REACT_APP_SERVER_URL}${currentUser.logo}`} alt="logo" style={{ width: '56px', borderRadius: '50%', marginLeft: '30px' }} />
             <Link onClick={UserLogoutHandler} className="headerLinkButton" to="/">
               Выход
             </Link>
@@ -72,6 +73,7 @@ export default function Header() {
           </Link>
           <div>
             <Link className="headerLinkButton" to="/profile/organization">{`${currentOrganization.name}`}</Link>
+            <img src={`${process.env.REACT_APP_SERVER_URL}${currentOrganization.logo}`} alt="logo" style={{ width: '56px', borderRadius: '50%', marginLeft: '30px' }} />
             <Link onClick={OrgLogoutHandler} className="headerLinkButton" to="/">
               Выход
             </Link>
