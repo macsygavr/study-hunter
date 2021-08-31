df57e2a06801085533f3721188afdcdd197049ca
Maksim Gavrilyuk <macsy.gavr.job@gmail.com>
2021-08-31 11:06:11 +0300

    home-styles-fix

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -6,12 +6,14 @@ function Posts({ resultToRender }) {
   // если прилетает пустой пропс, от рисуем, что постов нет или можно вообще ничего не рисовать
   const renderPosts = () => (resultToRender && resultToRender.length
     ? resultToRender.map((post) => (
-      <div key={post.id}><PostsItem {...post} /></div>
+      <div className="col-12 col-sm-6 col-md-4 mb-4" key={post.id}><PostsItem {...post} /></div>
     ))
     : <div>Здесь пока нет постов</div>);
   return (
-    <div className="d-flex flex-wrap" style={{ width: '80vw' }}>
-      {renderPosts()}
+    <div className="container">
+      <div className="row">
+        {renderPosts()}
+      </div>
     </div>
   );
 }
