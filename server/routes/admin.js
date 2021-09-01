781aa693bf8e88dbccdd91f40b6b4d6785400b48
Denis <d.ivanitskiy-1994@yandex.ru>
2021-09-01 15:42:29 +0300

    admin lk

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -16,10 +16,11 @@ router.patch('/changestatus', async (req, res) => {
 
 router.get('/bid', async (req, res) => {
   const allRequests = await db.Organization.findAll({raw: true}, {where: {is_checked: false, is_allowed: false}});
-  console.log(allRequests);
   const result = allRequests.map((item) => item = { 
     id: item.id,
     name: item.name,
+    is_checked: item.is_checked,
+    is_allowed: item.is_allowed,
   });
   res.json(result);
 });
@@ -35,6 +36,26 @@ router.get('/allusers', async (req, res) => {
     superadmin: item.superadmin,
   });
   res.json(result);
+});
+
+router.patch('/changebid', async (req, res) => {
+  if (req.body) {
+    const { status, id } = req.body;
+    const org = await db.Organization.findOne({ where: id });
+    if (status === 'accept') {
+      console.log(org);
+      await db.Organization.update( { is_checked: true, is_allowed: true }, { where: { id: org.id } });
+    }
+    else if (status === 'decline') {
+      await db.Organization.update( { is_checked: true, is_allowed: false }, { where: { id: org.id } });
+    } 
+    else {
+      res.sendStatus(400);
+    }
+    const newStatus = await db.Organization.findOne({ raw: true, where: id });
+    console.log(newStatus);
+    res.json(newStatus.is_checked);
+}
 });
 
 module.exports = router;
