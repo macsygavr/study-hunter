268d490bddea76bca642915e8cc6bbb3b3176fa8
lil-m00ha <minaevaan95@gmail.com>
2021-09-02 16:19:59 +0300

    requests now have modals

--- client/src/components/RequestsButton/RequestsButton.jsx
+++ client/src/components/RequestsButton/RequestsButton.jsx
@@ -2,20 +2,22 @@
 /* eslint-disable no-nested-ternary */
 import './requestButton.css';
 import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import axios from 'axios';
-import { addRequestUserStart } from '../../redux/actions/usersAC';
+// import { addRequestUserStart } from '../../redux/actions/usersAC';
 import PopUp from '../PopUp/PopUp';
+import ModalRequest from '../ModalRequest/ModalRequest';
 
-function RequestsButton({ userId, courseId }) {
-  const dispatch = useDispatch();
+function RequestsButton({ userId, courseId, courseName }) {
+  // const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state);
   const [popupActive, setPopupActive] = useState(false);
+  const [isReqModal, setIsReqModal] = useState(false);
   const [isRequested, setIsRequested] = useState(null);
 
-  const requestHandler = () => {
-    dispatch(addRequestUserStart(userId, courseId));
-  };
+  // const requestHandler = () => {
+  //   dispatch(addRequestUserStart(userId, courseId));
+  // };
 
   useEffect(() => {
     if (Object.keys(currentUser).length) {
@@ -30,11 +32,12 @@ function RequestsButton({ userId, courseId }) {
         <div className="alert alert-primary mt-3 mb-0 mx-auto pt-2" style={{ width: '180px', height: '38px' }}>Отклик отправлен</div>
       ) : Object.keys(currentUser).length
         ? (
-          <button onClick={requestHandler} type="button" className="btn btn-my-primary mt-3">Хочу здесь учиться!</button>
+          <button onClick={() => setIsReqModal(true)} type="button" className="btn btn-my-primary mt-3">Хочу здесь учиться!</button>
         ) : (
           <button onClick={() => setPopupActive(true)} type="button" className="btn btn-my-primary mt-3">Откликнуться!</button>
         )}
       <PopUp active={popupActive} setActive={setPopupActive} />
+      {isReqModal ? <ModalRequest setIsReqModal={setIsReqModal} userId={userId} courseId={courseId} courseName={courseName} /> : null}
     </>
   );
 }
